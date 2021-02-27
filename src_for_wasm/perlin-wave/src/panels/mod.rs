26e74b022f04f375977cf532e0f243577bb61e32
minagawah <minagawa@tokyo800.jp>
2021-02-27 16:27:10 +0900

    feat: Fixed Panel to take generic which implements Graphics

--- src_for_wasm/perlin-wave/src/panels/mod.rs
+++ src_for_wasm/perlin-wave/src/panels/mod.rs
@@ -3,11 +3,10 @@ pub mod wave;
 use core::cell::RefCell;
 use std::rc::Rc;
 
-use crate::graphics::wave::WaveGraphics;
 use crate::graphics::Graphics;
 
-pub trait Panel {
-    fn g(&self) -> Rc<RefCell<WaveGraphics>>;
+pub trait Panel<G: Graphics> {
+    fn g(&self) -> Rc<RefCell<G>>;
 
     fn reset(&mut self) {
         if let Ok(mut g) = self.g().try_borrow_mut() {
