162639bbf30f1f5f06cbe2e3830421710088e26a
minagawah <minagawa@tokyo800.jp>
2021-02-28 12:57:20 +0900

    refactor: Changed the field name

--- src_for_wasm/perlin-wave/src/types.rs
+++ src_for_wasm/perlin-wave/src/types.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use std::f64::consts::PI;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct PanelConfig {
@@ -24,39 +23,6 @@ impl Point {
         Point {
             x: (self.x + 1.0) / 2.0,
             y: (self.y + 1.0) / 2.0,
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct Solar {
-    pub segments: f64,
-    pub margin: f64,
-    pub radius: f64,
-    pub radius_inner: f64,
-    pub max_length: f64,
-    pub size: f64,
-    pub angle_step: f64,
-}
-
-impl Solar {
-    pub fn new(canvas_height: f64, segments: f64) -> Self {
-        let angle_step = 360.0 / segments;
-        let diameter = canvas_height * 0.9;
-        let margin = (canvas_height - diameter) / 2.0;
-        let radius = diameter / 2.0;
-        let radius_inner = radius * 0.65;
-        let max_length = radius - radius_inner;
-        let size = diameter * PI / segments * 0.2;
-
-        Solar {
-            segments,
-            margin,
-            radius,
-            radius_inner,
-            max_length,
-            size,
-            angle_step,
         }
     }
 }
